69eb1e34310ebb9863e19e74f065ca6de6652dc4
JG-Dev <ljg615@nate.com>
2020-03-15 23:06:09 +0900

    파라메터 재사용가능하도록 수정 incrementer(new RunIdIncrementer()) 추가하면 되는가봄

--- src/main/java/com/jg/dev/job/ReadJobConfiguration.java
+++ src/main/java/com/jg/dev/job/ReadJobConfiguration.java
@@ -5,7 +5,6 @@ import com.jg.dev.batch.UserReader;
 import com.jg.dev.batch.UserWriter;
 import com.jg.dev.listener.UserListener;
 import com.jg.dev.model.User;
-import com.jg.dev.repository.DevRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -13,7 +12,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -38,6 +37,7 @@ public class ReadJobConfiguration {
         return jobBuilderFactory.get(JOB_NAME)
                 .listener(userListener)
                 .start(step())
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
--- src/test/java/com/jg/dev/jobs/JobTests.java
+++ src/test/java/com/jg/dev/jobs/JobTests.java
@@ -1,11 +1,9 @@
-package com.jg.dev;
+package com.jg.dev.jobs;
 
 import com.jg.dev.job.ReadJobConfiguration;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -14,28 +12,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDateTime;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestPropertySource(properties = {"job.name=" + ReadJobConfiguration.JOB_NAME})
-public class JobTest {
+public class JobTests {
 
     private JobLauncherTestUtils jobLauncherTestUtils;
 
     @Test
     public void 배치테스트() throws Exception{
-//        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
-//
-//        assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
-
         try {
             JobParameters jobParameters = new JobParametersBuilder()
-                    .addString("date", LocalDateTime.now().toString())
+                    .addString("startTxDate", "2020-03")
+                    .addString("nextTxDate", "2020-03-15")
+                    .addLong("time", System.currentTimeMillis())
                     .toJobParameters();
 
             JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
